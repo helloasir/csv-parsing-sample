4d1770f2a22a35f7588e75a59a28aeb35145ebe4
Levy Srugo <41137165+lsrugo@users.noreply.github.com>
2022-02-01 06:34:05 +0000

    add netlify function

---
+++ netlify/functions/hello.js
@@ -0,0 +1,7 @@
+// https://docs.netlify.com/functions/build-with-javascript/
+exports.handler = async function (event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
+    };
+  }
